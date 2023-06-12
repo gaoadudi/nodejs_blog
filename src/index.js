3303c57c849aaa5c0342daab3b4e5ef28b632dd5
hieupt <phanhieucf@gmail.com>
2023-06-12 20:54:58 +0700

    update

--- src/index.js
+++ src/index.js
@@ -57,5 +57,5 @@ app.use(session({
 // Init routes
 route(app);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`> App listening on port ${port}`)); // localhost:3000
